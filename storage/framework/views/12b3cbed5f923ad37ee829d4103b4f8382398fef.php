f427ec95f3b2effc465a925c889ae80ad165eb8e
DESKTOP-QEQPJ4V\Kepoyzzz <kds123babi@gmail.com>
2018-12-28 00:26:42 +0700

    new

--- storage/framework/views/12b3cbed5f923ad37ee829d4103b4f8382398fef.php
+++ storage/framework/views/12b3cbed5f923ad37ee829d4103b4f8382398fef.php
@@ -4,32 +4,58 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="./css/job.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="./job.css">
     <link rel="stylesheet" type="text/css" href="<?php echo e(asset('css/style.css')); ?>">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>"Bantu Semua" - <?php echo $__env->yieldContent('title'); ?></title>
+    <title>Bantu Semua <?php echo $__env->yieldContent('title'); ?></title>
 </head>
 <body>
-    <nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/">Bantusemua.com</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Job
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="/showjob" id="showjob">View Job List</a>
+                    <a class="dropdown-item" href="/job">Add Job</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Forum</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Donation</a>
+            </li>
+        </ul>
+    </div>
+    </nav>
+    <!-- <nav class="navbar navbar-inverse">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="/">Help-Out</a>
+                <a class="navbar-brand" href="/">Bantu Semua</a>
             </div>
             <ul class="nav navbar-nav">
                 <li class="active"><a href="/">Home</a></li>
                 <li><a href="/job" id="job">Input Job</a></li>
-                <li><a href="/showjob" id="showjob">JobList</a></li>
+                <li></li>
             </ul>
         </div>
-    </nav>
-    <nav><a href="/" id="Home">Home</a></nav>
+    </nav> -->
+    <!-- <nav><a href="/" id="Home">Home</a></nav>
     <nav><a href="/register" id="register">Register User</a></nav>
     <nav><a href="/registerAdmin" id="register">Register Admin</a></nav>
     <nav><a href="/login" id="login">Login User</a></nav>
-    <nav><a href="/loginAdmin" id="login">Login Admin</a></nav>
+    <nav><a href="/loginAdmin" id="login">Login Admin</a></nav> -->
     <main>
         <?php echo $__env->yieldContent('main'); ?>
     </main>
